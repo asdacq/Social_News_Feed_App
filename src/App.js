c218d7e810b13c8988db05f619d121a7582c2bfa
asdacq <hhuang0815@gmail.com>
2018-06-06 21:14:53 -0400

    added things from class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import doge from "./doge.jpg"
+import pepe from "./pepe.png"
+import knuckles from "./knuckles.jpg"
+
+var test = [];
+test.push({greeting: "Hello"});
+
+
+const cards = [];
+cards.push({image: "./pepe.png", caption: "Make America Great Again", author: "TrumpFan101"});
+cards.push({image: "./doge.jpg", caption: "Such awesome, very wow, so goodness", author: "goodboy"});
+cards.push({image: "./knuckles.jpg", caption: "Do u knoe da way", author: "Kony2012"});
+
+
 
 class App extends Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            cards: []
+        }
+    }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            <div className = "border">
+                <img className = "image" src = {pepe} alt = "pepe"></img>
+                <p>{cards[0].author}</p>
+                <p>{cards[0].caption}</p>
+            </div>
+            <div className = "border">
+                <img className = "image" src = {doge} alt = "doge"></img>
+                <p>{cards[1].author}</p>
+                <p>{cards[1].caption}</p>
+            </div>
+            <div className = "border">
+                <img className = "image" src = {knuckles} alt = "knuckles"></img>
+                <p>{cards[2].author}</p>
+                <p>{cards[2].caption}</p>
+            </div>
       </div>
     );
   }
